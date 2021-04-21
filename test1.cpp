7fd3a1d8249feeef845e4d4ca5278f5e8de6e15e
WikelBimaLeonardo <wikelbimaleonardo@gmail.com>
2021-04-21 22:24:52 +0700

    Menambahkan file Test1.cpp

---
+++ test1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 4, b = 3;
+
+    int x = a+b;
+    int y = a-b;
+
+    cout << "Hasil Penjumlahan :" << x;
+    cout << endl;
+    cout << "Hasil Pengurangan :" << y;
+    
+    return 0;
+}
